26f7dd0feaa280eb44383b1755f4e41d9e7f03d4
YPermitin <ypermitin@yandex.ru>
2020-12-19 23:55:23 +0500

    Добавлен служебный класс для работы со строками в проект unit-тестов

---
+++ Tests/YY.EventLogReaderAssistant.Tests/Helpers/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YY.EventLogReaderAssistant.Tests.Helpers
+{
+    public static class StringExtensions
+    {
+        public static DateTime ToDateTime(this string sourceValue)
+        {
+            try
+            {
+                return DateTime.Parse(sourceValue);
+            }
+            catch
+            {
+                return DateTime.MinValue;
+            }
+        }
+    }
+}
